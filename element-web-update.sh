1d6ea1ade099fb7e53b29d4ab9ba7ef894671a22
Alexey Murz Korepov <murznn@gmail.com>
2021-09-16 06:23:54 +0300

    Overwriting variables using .env file

--- element-web-update.sh
+++ element-web-update.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 ###################################################################
-# Script for check new version of Element from github
+# Script for check new version of Element from GitHub
 # and download new version, if update is avaiable
 #
-# https://gist.github.com/MurzNN/ee64f98ab2e71b886c41d55594e5dd9e
+# https://github.com/MurzNN/element-web-update
 #
 ###################################################################
 
+# You can override those variables using .env file in your directory
+
 # Directory where Element files must be placed
 DIRECTORY_INSTALL=~/public_html
+
 # Directory for temp files - must be different than install directory!
 DIRECTORY_TMP=/tmp
 
+# Url to repo for check version
 VERSION_URL=https://api.github.com/repos/vector-im/element-web/releases/latest
+
+if [ -f ".env" ]; then source .env; fi
+
+VERSION_INSTALLED=`if [ -f "$DIRECTORY_INSTALL/version" ]; then cat $DIRECTORY_INSTALL/version; else echo "null"; fi`
+VERSION_LATEST=`curl -s $VERSION_URL | jq -r '.name' | sed s/v//` || { echo "Error checking last Element version!"; exit 1; }
 
 command -v curl >/dev/null 2>&1 || { echo "You need to install "curl" package for this script: sudo apt install curl"; exit 1; }
 command -v tar >/dev/null 2>&1 || { echo "You need to install "tar" package for this script: sudo apt install tar"; exit 1; }
 command -v jq >/dev/null 2>&1 || { echo "You need to install "jq" package for this script: sudo apt install jq"; exit 1; }
-
-VERSION_INSTALLED=`cat $DIRECTORY_INSTALL/version`
-VERSION_LATEST=`curl -s $VERSION_URL | jq -r '.name' | sed s/v//` || { echo "Error checking last Element version!"; exit 1; }
 
 if ( [[ -z "$VERSION_LATEST" ]] || [ "$VERSION_LATEST" == "null" ] ); then
   echo "Error! Received bad version number from $VERSION_URL: $VERSION_LATEST"
